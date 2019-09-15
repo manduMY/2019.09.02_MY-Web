1e2669cba4f716697cb197b1ff8151ef9d6079da
manduMY <poi31320@gmail.com>
2019-09-15 19:14:56 +0900

    ejs warning: Troubleshooting retrieving ejs pages from iframe

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
 
 app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname, '/')));
- 
+app.use(express.static(path.join(__dirname,'/')));
 
+app.get('/', function(req,res){
+  res.render('index');
+  //회원가입 페이지로 접근
+
+});
+// app.get('/main', (req, res) => {
+//   res.render('main',data)
+// });
+app.get('/main', (req, res) => {
+  res.render('main', {
+  })
+});
 app.listen(3000, function(){
   console.log('Server On!');
 });
