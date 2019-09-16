4b4b499f71e7947b9d80f1e7ed1a5c83dc04983c
manduMY <poi31320@gmail.com>
2019-09-16 23:16:32 +0900

    makingweb page add!!

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var http = require('http');
+var fs = require('fs');
+
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -10,13 +13,15 @@ app.get('/', function(req,res){
   //회원가입 페이지로 접근
 
 });
-// app.get('/main', (req, res) => {
-//   res.render('main',data)
-// });
 app.get('/main', (req, res) => {
   res.render('main', {
+  })
+});
+app.get('/makingweb', (req, res) => {
+  res.render('makingweb', {
   })
 });
 app.listen(3000, function(){
   console.log('Server On!');
 });
+
