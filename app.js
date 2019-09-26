10424a66e1d8880ed93b92298ee215c5bd104ff7
manduMY <poi31320@gmail.com>
2019-09-26 15:21:03 +0900

    sign up error handling

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var fs = require('fs');
 var mongoose = require("mongoose");
 var express = require('express');
 var path = require('path');
-var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require("method-override");
+var flash = require("connect-flash");
+var session = require("express-session");
+var app = express();
 
 // DB setting
 // 사용 중단이 뜨는 DB오류가 뜰경우 아래 코드 3줄 입력
@@ -33,6 +35,8 @@ app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(flash());
+app.use(session({secret:"MySecret", resave:true, saveUninitialized:true}));
 
 // Routes
 app.use("/", require("./routes/home"));
